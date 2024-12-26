8ef44754227b6d448c2208eab7897dccec6f31f0
Damoshh <adamrujhan@gmail.com>
2024-12-26 21:59:15 +0800

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import Header from './Header.jsx'
+import Navbar from './component/Navbar.jsx'
+import Footer from './component/Footer.jsx'
+import Home from "./pages/Home.jsx"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
-    
-  return(
-    <Header/>
-  );
-}
+    return(
+        <div className='App'>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element = {<Home />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
+}   
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import BannerImage from '../assets/filter.png'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className='home' style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div 
+        className = "headerContainer" 
+      >
+        <h1>Hai</h1>
+        <p>eat</p>
+        <Link to="/about">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home
